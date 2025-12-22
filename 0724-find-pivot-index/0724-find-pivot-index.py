1487201e4c6730706b32d1c85cd9964e4e8d80f8
l-m-neeraja <151540886+l-m-neeraja@users.noreply.github.com>
2025-12-22 20:27:50 +0530

    Time: 7 ms (60.14%), Space: 13.6 MB (14.67%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        p=[nums[0]]
+        for i in range(1,len(nums)):
+            p.append(p[i-1]+nums[i])
+        if p[-1]-p[0]==0:
+            return 0
+        for i in range(1,len(nums)):
+            if i==len(nums)-1:
+                if p[-2]==0:
+                    return i
+            elif p[i-1]==p[-1]-p[i]:
+                return i
+        return -1
