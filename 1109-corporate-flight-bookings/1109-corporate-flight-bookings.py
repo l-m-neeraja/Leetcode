82b341fcef58481446b2713a693b0c9ecb333c73
l-m-neeraja <151540886+l-m-neeraja@users.noreply.github.com>
2025-12-22 21:36:04 +0530

    Time: 31 ms (34.25%), Space: 22.6 MB (17.81%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def corpFlightBookings(self, bookings, n):
+        """
+        :type bookings: List[List[int]]
+        :type n: int
+        :rtype: List[int]
+        """
+        res=[0]*(n+1)
+        for i,j,k in bookings:
+            res[i-1]+=k
+            if j<n:
+                res[j]-=k
+        for i in range(1,len(res)):
+            res[i]+=res[i-1]
+        return res[:n]
