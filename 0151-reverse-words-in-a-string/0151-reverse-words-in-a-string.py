e67627d74fe09e932bfe961e087faef325d4f2db
l-m-neeraja <151540886+l-m-neeraja@users.noreply.github.com>
2026-01-31 15:52:10 +0530

    Time: 0 ms (100%), Space: 12.6 MB (68.1%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -4,14 +4,5 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        s=s.lstrip()
-        s=s.rstrip()
-        res=""
-        temp=""
-        for i in range(len(s)-1,-1,-1):
-            if s[i]==" " and temp and temp[-1]!=" ":
-                res+=temp[::-1]+" "
-                temp=""
-            elif s[i]!=" ":
-                temp+=s[i]
-        return res+temp[::-1]
+        lst=s.split()
+        return " ".join(lst[::-1])
